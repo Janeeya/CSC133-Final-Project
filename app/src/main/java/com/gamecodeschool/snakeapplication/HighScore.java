14058015b253353dbcc0a2f052ddfbd2575f9604
ivangutierrez <ivan.gutierrez19971@gmail.com>
2024-05-08 22:29:26 -0700

    comparing and storing new high score

--- app/src/main/java/com/gamecodeschool/snakeapplication/HighScore.java
+++ app/src/main/java/com/gamecodeschool/snakeapplication/HighScore.java
@@ -1,27 +1,35 @@
 package com.gamecodeschool.snakeapplication;
+
 import android.content.Context;
 import android.content.SharedPreferences;
+
 public class HighScore {
     private static final String PREF_NAME = "SnakeHighScores";
     private static final String KEY_HIGH_SCORE = "high_score";
 
-    private SharedPreferences mSharedPreferences;
+    private final SharedPreferences mSharedPreferences;
+
+    private int mHighScore;
 
     public HighScore(Context context) {
         mSharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        mHighScore = mSharedPreferences.getInt(KEY_HIGH_SCORE, 0);
     }
 
     public int getHighScore() {
-        return mSharedPreferences.getInt(KEY_HIGH_SCORE, 0);
+        return mHighScore;
     }
 
     public void setHighScore(int score) {
-        SharedPreferences.Editor editor = mSharedPreferences.edit();
-        editor.putInt(KEY_HIGH_SCORE, score);
-        editor.apply();
+        if (score > mHighScore) {
+            SharedPreferences.Editor editor = mSharedPreferences.edit();
+            editor.putInt(KEY_HIGH_SCORE, score);
+            editor.apply();
+            mHighScore = score;
+        }
     }
 
     public boolean isNewHighScore(int score) {
-        return score > getHighScore();
+        return score > mHighScore;
     }
 }
