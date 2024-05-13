227a6157c802affc42a0165d498821d3a541c62c
ivangutierrez <ivan.gutierrez19971@gmail.com>
2024-05-12 23:14:04 -0700

    extracted createSoundPool and loudSound to handle specific tasks, switch statements instead of if-else

--- app/src/main/java/com/gamecodeschool/snakeapplication/SoundEffectStrategy.java
+++ app/src/main/java/com/gamecodeschool/snakeapplication/SoundEffectStrategy.java
@@ -2,44 +2,54 @@ package com.gamecodeschool.snakeapplication;
 
 import android.content.Context;
 import android.media.AudioAttributes;
-import android.media.AudioManager;
 import android.media.SoundPool;
 
 public class SoundEffectStrategy implements IAudioStrategy {
-    private Context context;
     private SoundPool soundPool;
-    private int eatSoundId;
-    private int crashSoundId;
-    private int slowmoSoundId;
+    private final int eatSoundId;
+    private final int crashSoundId;
+    private final int slowmoSoundId;
 
     public SoundEffectStrategy(Context context) {
-        this.context = context;
+        this.soundPool = createSoundPool();
+        this.eatSoundId = loadSound(context, R.raw.biscuit_bark);
+        this.crashSoundId = loadSound(context, R.raw.dog_death);
+        this.slowmoSoundId = loadSound(context, R.raw.slowmo);
+    }
 
+    private SoundPool createSoundPool() {
         AudioAttributes audioAttributes = new AudioAttributes.Builder()
                 .setUsage(AudioAttributes.USAGE_GAME)
                 .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
                 .build();
 
-        soundPool = new SoundPool.Builder()
+        return new SoundPool.Builder()
                 .setMaxStreams(5)
                 .setAudioAttributes(audioAttributes)
                 .build();
+    }
 
-        eatSoundId = soundPool.load(context, R.raw.biscuit_bark, 1);
-        crashSoundId = soundPool.load(context, R.raw.dog_death, 1);
-        slowmoSoundId = soundPool.load(context, R.raw.slowmo, 1);
+    private int loadSound(Context context, int soundResource) {
+        return soundPool.load(context, soundResource, 1);
     }
 
     @Override
     public void apply(GameEventType eventType) {
-        if (eventType == GameEventType.EAT) {
-            soundPool.play(eatSoundId, 1.0f, 1.0f, 1, 0, 1.0f);
-        } else if (eventType == GameEventType.CRASH) {
-            soundPool.play(crashSoundId, 1.0f, 1.0f, 1, 0, 1.0f);
-        } else if (eventType == GameEventType.POWERUP) {
-            soundPool.play(slowmoSoundId, 1.0f, 1.0f, 1, 0, 1.0f);
-
+        switch (eventType) {
+            case EAT:
+                playSound(eatSoundId);
+                break;
+            case CRASH:
+                playSound(crashSoundId);
+                break;
+            case POWERUP:
+                playSound(slowmoSoundId);
+                break;
         }
+    }
+
+    private void playSound(int soundId) {
+        soundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
     }
 
     @Override
@@ -50,3 +60,4 @@ public class SoundEffectStrategy implements IAudioStrategy {
         }
     }
 }
+
